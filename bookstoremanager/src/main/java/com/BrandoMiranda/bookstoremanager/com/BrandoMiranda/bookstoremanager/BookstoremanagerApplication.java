a92d58f3f8154a0c1d43719f0b01fff3baef32c6
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-03-23 21:16:22 -0300

    First commit: project structure created

---
+++ bookstoremanager/src/main/java/com/BrandoMiranda/bookstoremanager/com/BrandoMiranda/bookstoremanager/BookstoremanagerApplication.java
@@ -0,0 +1,14 @@
+package com.BrandoMiranda.bookstoremanager.com.BrandoMiranda.bookstoremanager;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanagerApplication.class, args);
+	}
+
+}
