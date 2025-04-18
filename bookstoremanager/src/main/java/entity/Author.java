48838b14a223cf82bfab9e8ba00abb67d0c09b01
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-04-18 18:44:23 -0300

    changing the book and author class

--- bookstoremanager/src/main/java/entity/Author.java
+++ bookstoremanager/src/main/java/entity/Author.java
@@ -1,10 +1,29 @@
 package entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer age;
+	
+	
 	}
 
-}
+
--- bookstoremanager/src/main/java/entity/Book.java
+++ bookstoremanager/src/main/java/entity/Book.java
@@ -1,6 +1,40 @@
 package entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer pages;
+	
+	@Column(nullable = false)
+	private Integer chapters;
+	
+	@Column(nullable = false)
+	private String isbn;
+	
+	@Column(name = "publisher_name", nullable = false, unique = true)
+	private String publisherName;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+	@JoinColumn(name = "author_id")
+	private Author author;
 }
+
+
